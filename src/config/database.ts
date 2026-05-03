52c23795bd3081470cb5129029c51dadba16a020
Andres Barragan <al22760549@ite.edu.mx>
2026-05-02 21:45:48 -0700

    Error de driver de prisma arreglado

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { env } from './env'
+
+const pool = new Pool({ connectionString: env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ log: ['error'] })
+  globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
