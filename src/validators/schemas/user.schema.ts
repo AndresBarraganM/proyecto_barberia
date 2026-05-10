12a0e92f2dda202bcf85fc17c9e052f02275a965
Andres Barragan <al22760549@ite.edu.mx>
2026-05-10 16:05:41 -0700

    verificacion de usuaria refragmentada para utilizar esquemas que pueden ser reutilizados

---
+++ src/validators/schemas/user.schema.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod'
+
+// roles aceptados para usuarios
+const roles = ['cliente', 'estilista', 'recepcionista', 'admin'] as const
+
+export const userSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(2, 'El nombre debe tener al menos 2 caracteres')
+    .max(50, 'El nombre no puede superar los 50 caracteres'),
+
+  email: z
+    .email('Email inválido'),
+
+  contrasena: z
+    .string()
+    .min(8, 'La contraseña debe tener al menos 8 caracteres')
+    .max(50, 'La contraseña no puede superar los 50 caracteres')
+    // un caracter especial, una mayúscula, una minúscula y un número
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*i\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      'La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial'
+    ),
+
+    role: z.enum(roles, {
+      message: `El rol debe ser uno de los siguientes: ${roles.join(', ')}`,
+    }),
+
+  telefono: z
+    .string()
+    .trim()
+    .min(10, 'El teléfono debe tener al menos 10 caracteres')
+    .max(15, 'El teléfono no puede superar los 15 caracteres'),
+})
