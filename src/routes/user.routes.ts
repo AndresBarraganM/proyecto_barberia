a999db5fca3d6a9b3ccd24ad1c08b897ac244035
Andres Barragan <al22760549@ite.edu.mx>
2026-05-27 18:38:10 -0700

    eliminando rutas que no usamos por que generar error

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,12 @@
+
 import { Router } from 'express'
-import { getAll, getById, update, remove } from '../controllers/user.controller'
+//import { getAll, getById, update, remove } from '../controllers/user.controller'
 import { authenticate, authorize } from '../middlewares/auth.middleware'
 import { validate } from '../middlewares/validate.middleware'
 import { updateUserSchema } from '../validators/user.validator'
 
 const router = Router()
-
+/* 
 // Todas las rutas de usuarios requieren autenticación
 router.use(authenticate)
 
@@ -20,5 +21,6 @@ router.patch('/:id', validate(updateUserSchema), update)
 
 // DELETE /api/users/:id  — solo Admin
 router.delete('/:id', authorize('ADMIN'), remove)
-
+ */
 export default router
+
